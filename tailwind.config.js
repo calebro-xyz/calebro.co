427c9f23bbef66bc7461b1dcd6d40b39f37428cb
calebro <calebro@localhost.localdomain>
2022-03-09 17:08:35 +0300

    transfer to legion

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,6 @@ module.exports = {
     plugins: [],
 }
 module.exports = {
-
-    daisyui: {
-        themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade"],
-    },
+    //...
+    plugins: [require("daisyui")],
 }
